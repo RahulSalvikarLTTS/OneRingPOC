c57fe2f757b7435db82e488809d550f5ae702e58
ravichandrabm1980 <rahul.salvikar@ltts.com>
2023-03-09 19:42:19 +0530

    Working on Unittesting

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,8 +4,8 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../src/LoginDemo/App';
-
+//import App from '../src/LoginDemo/App';
+import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
